dc8e31d62be0385e726f4414e54dd7dbe30b485d
laxa <laxa@ddracepro.net>
2016-01-19 23:44:04 +0100

    Background added to configuration with deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ cp bashrc ~/.bashrc
 cp gitconfig ~/.gitconfig
 cp keyboardshortcut.xml ~/.gconf/apps/gnome-terminal/keybindings/%gconf.xml
 cp terminal.xml ~/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
+cp last-edited.xml ~/.config/gnome-control-center/backgrounds/
 cp background.jpg ~/Documents/
 cp screenrc ~/.screenrc
 cp user ~/.config/dconf/user
