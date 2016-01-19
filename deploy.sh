cc2950ee043e1d75dce5ee687f076efea1c79d36
Julien EGLOFF <laxa@ddracepro.net>
2016-01-19 23:15:03 +0100

    Update mistake on deploy path of terminal settings

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ cp emacs ~/.emacs
 cp bashrc ~/.bashrc
 cp gitconfig ~/.gitconfig
 cp keyboardshortcut.xml ~/.gconf/apps/gnome-terminal/keybindings/%gconf.xml
-cp terminal.xml ~/.gconf/apps/gnome-terminal/keybindings/%gconf.xml
+cp terminal.xml ~/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
 cp background.jpg ~/Documents
 cp user ~/.config/dconf/user
