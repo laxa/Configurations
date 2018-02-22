e87512e2717d92e3515d0bfe8c615a7525833f24
Julien EGLOFF <laxa@ddracepro.net>
2018-02-22 10:25:46 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-apt-get update && apt-get dist-upgrade $1 && apt-get autoremove $1
+apt update && apt dist-upgrade $1 && apt autoremove $1
 if [ -f /var/run/reboot-required ]; then
     echo 'reboot required'
 fi
